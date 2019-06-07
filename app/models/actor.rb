9bc8ae76f27b96b893f5ddb07f60e7200e01f5b6
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-06-07 00:25:57 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -9,7 +9,7 @@ class Actor < ActiveRecord::Base
   
   def list_roles
     self.characters.collect do |characters|
-      "#{characters.name} #{characters.show}"
+      "#{name} #{show.name}"
     end
   end
 end
